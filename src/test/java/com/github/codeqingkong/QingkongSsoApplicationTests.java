88e1f26da29fc995b5e885d66ef66ccc300cf30d
LuoZhiWei <1031556593@qq.com>
2022-01-01 17:09:15 +0800

    项目初始化

---
+++ src/test/java/com/github/codeqingkong/QingkongSsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.codeqingkong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QingkongSsoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
